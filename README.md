03eaf5133fbbe069026fbea47382bd5ae50b2a73
Brandon Bayer <b@bayer.ws>
2020-09-29 21:29:15 -0400

    files

---
+++ README.md
@@ -0,0 +1,13 @@
+[![Blitz.js](https://raw.githubusercontent.com/blitz-js/art/master/github-cover-photo.png)](https://blitzjs.com)
+
+This is a [Blitz.js](https://github.com/blitz-js/blitz) app.
+
+# Example Sentry Integration
+
+
+Main integrations:
+- `blitz.config.js`
+- `app/pages/_app.tsx`
+- `app/pages/_error.tsx`
+- `app/components/ErrorComponent`
+- `integrations/sentry`
---
+++ app/pages/_app.tsx
@@ -0,0 +1,53 @@
+import Sentry from "integrations/sentry"
+import { useEffect } from "react"
+import { AppProps, useRouter, useSession } from "blitz"
+import { ErrorBoundary, FallbackProps } from "react-error-boundary"
+import { queryCache } from "react-query"
+import ErrorComponent from "app/components/ErrorComponent"
+
+export default function App({ Component, pageProps, err }: AppProps & { err: any }) {
+  const session = useSession()
+  const getLayout = Component.getLayout || ((page) => page)
+  const router = useRouter()
+
+  useEffect(() => {
+    Sentry.setUser({ id: session.userId.toString() })
+  }, [session])
+
+  return (
+    <ErrorBoundary
+      onError={(error, componentStack) => {
+        Sentry.captureException(error, { contexts: { react: { componentStack } } })
+      }}
+      FallbackComponent={RootErrorFallback}
+      resetKeys={[router.asPath]}
+      onReset={() => {
+        // This ensures the Blitz useQuery hooks will automatically refetch
+        // data any time you reset the error boundary
+        queryCache.resetErrorBoundaries()
+      }}
+    >
+      {getLayout(<Component {...pageProps} err={err} />)}
+    </ErrorBoundary>
+  )
+}
+
+function RootErrorFallback({ error, resetErrorBoundary }: FallbackProps) {
+  if (error?.name === "AuthenticationError") {
+    return <h1>Login form</h1>
+  } else if (error?.name === "AuthorizationError") {
+    return (
+      <ErrorComponent
+        statusCode={(error as any).statusCode}
+        title="Sorry, you are not authorized to access this"
+      />
+    )
+  } else {
+    return (
+      <ErrorComponent
+        statusCode={(error as any)?.statusCode || 400}
+        title={error?.message || error?.name || "Unknown error"}
+      />
+    )
+  }
+}
